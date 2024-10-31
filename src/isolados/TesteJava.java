167b750aae2b66eff8f5fca856b6bc30c804786d
lucas.ferreira <lucas.bref82@gmail.com>
2024-10-31 12:06:22 -0300

    Teste predicado

---
+++ src/isolados/TesteJava.java
@@ -0,0 +1,26 @@
+package isolados;
+
+import java.util.regex.Pattern;
+
+public class TesteJava {
+    private static final Pattern PATTERN = Pattern.compile("^S \\d+$");
+
+    public static void main(String[] args) {
+
+        String teste1 = "123456";
+        String teste2 = "ABSC#12";
+        String teste3 = "S 123";
+        String teste4 = "S123";
+        String teste5 = "";
+
+        System.out.println(padraoAutorizacaoFrete(teste1));
+        System.out.println(padraoAutorizacaoFrete(teste2));
+        System.out.println(padraoAutorizacaoFrete(teste3));
+        System.out.println(padraoAutorizacaoFrete(teste4));
+        System.out.println(padraoAutorizacaoFrete(teste5));
+    }
+
+    public static boolean padraoAutorizacaoFrete(String valor) {
+        return PATTERN.asPredicate().test(valor);
+    }
+}
