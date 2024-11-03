0d3add9f2612d1fd5c51c0da5382e14e2ab351d9
lucas.ferreira <lucas.bref82@gmail.com>
2024-11-03 14:05:17 -0300

    Exercicio 09

---
+++ src/repetitivas/Exercicio09.java
@@ -0,0 +1,16 @@
+package repetitivas;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            // Usando o Math.pow (Casting obrigando a não ter casas decimais
+            System.out.println(i + " " + (int) Math.pow(i, 2) + " " + (int) Math.pow(i, 3));
+        }
+        sc.close();
+    }
+
+}
