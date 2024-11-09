ee4d234d1fa470b336404604cb12a32f4319087e
lucas.ferreira <lucas.bref82@gmail.com>
2024-11-09 17:02:31 -0300

    Identação

--- src/orientacaoobjetos/arranjos/exercicio01/Application.java
+++ src/orientacaoobjetos/arranjos/exercicio01/Application.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Digite a quantidade de alturas a serem lidas: ");
         int quantidadeAlturas = scanner.nextInt();
         double[] totalAlturas = new double[quantidadeAlturas];
+
         for(int i = 0; i < quantidadeAlturas; i++) {
             totalAlturas[i] = scanner.nextDouble();
         }
+
         double somaAltura = 0;
         for (Double valor : totalAlturas) {
             somaAltura += valor;
